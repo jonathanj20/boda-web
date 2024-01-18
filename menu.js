a6537a2c0f98f2b0f8693d108bd9a1a0ecf329dd
jonathanj20 <jonathanpossada@gmail.com>
2024-01-18 00:55:01 -0700

    Se puso el menú horizontal y se ajustaron cambios en diseño

--- menu.js
+++ menu.js
@@ -1,11 +1,25 @@
 const btnOpen = document.getElementById('btnOpen');
 const btnClose = document.getElementById('btnClose');
 const nav = document.getElementById('nav');
+const links = document.querySelectorAll('.link-nav')
 
 btnOpen.addEventListener("click", () => {
     nav.style.display = 'block';
+    btnClose.style.display = 'block'
+    btnOpen.style.display = 'none';
 });
 
 btnClose.addEventListener("click", () => {
+    btnOpen.style.display = 'block';
     nav.style.display = 'none';
+    btnClose.style.display = 'none';
 });
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.style.display = 'none';
+        btnClose.style.display = 'none';
+        btnOpen.style.display = 'block';
+    });
+});
+
